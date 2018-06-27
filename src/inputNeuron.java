e710643ab91a8de35cef416407e65adc1e3c4518
Hangus95 <32011564+Hangus95@users.noreply.github.com>
2018-06-27 09:38:17 +0200

    Update inputNeuron.java

--- src/inputNeuron.java
+++ src/inputNeuron.java
@@ -1,10 +1,5 @@
 
-/**
- * Write a description of class inputNeuron here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class inputNeuron extends Neuron
 {
     public inputNeuron()
