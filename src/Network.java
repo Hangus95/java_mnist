ab4eb69bafdfc8f7a28449e395bd207c021ee236
Hangus95 <32011564+Hangus95@users.noreply.github.com>
2018-06-27 09:40:21 +0200

    Update Network.java

--- src/Network.java
+++ src/Network.java
@@ -35,7 +35,6 @@ public class Network
         
         createNetwork( noInputs,  noHidden,  noOutputs);
         createConnections();
-        //data = new ArrayList<String>();
         prepData();
         
         
