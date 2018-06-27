0b92c6abee82b42e31bc39091dff4c1891b6ced2
Hannes Gustafsson <>
2018-06-27 08:51:39 +0200

    ON

---
+++ outputNeuron.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class outputNeuron here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class outputNeuron extends Neuron
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class outputNeuron
+     */
+    public outputNeuron()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+  
+}
